e438346eb973d817e443e37e8bd0a244cf6de05a
arilfrankel <ari.l.frankel@gmail.com>
2018-03-01 19:04:25 -0600

    more robust parrot request identification

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 /**
- * @todo more robust findParrotRequest
+ * @todo cache to handle redundant Slack events
 */
 const http = require('http');
 const { createMessageApi: createMessage, postMessage } = require('./bot');
 const { consoleOut } = require('./util');
 
 const isChallenge = body => Boolean(body.challenge);
-const findParrotRequest = ({ event: { text, channel } }) => ({
-  user: text.match(/<@\w{9}>(?!.*<@\w{9}>)/),
-  channel,
+const findParrotRequest = ({ event }) => Object.assign(event, {
+  user: (/<@U9DM0PHJL> parrot <@\w{9}>/).test(event.text)
+    ? event.text.match(/<@\w{9}>(?!.*<@\w{9}>)/)
+    : null,
 });
 
 const port = process.env.PORT || 8080;
@@ -38,10 +39,18 @@ http.createServer((request, response) => {
       response.setHeader('Content-Type', 'application/x-www-form-urlencoded');
       return response.end(body.challenge);
     }
+    // const { event_id: eventId } = body;
+    // console.log('id', eventId, cache.has(eventId));
+    // // Handle each message only once...Slack seems to send them multiple times
+    // if (cache.has(eventId)) return response.end();
+    // cache.add(eventId);
+    // setTimeout(() => {
+    //   cache.delete(eventId)
+    // }, 10000);
     const parrotRequest = findParrotRequest(body);
+    const { channel } = parrotRequest;
     if (parrotRequest.user !== null) {
       const userId = parrotRequest.user[0].slice(2, 11);
-      const { channel } = parrotRequest;
       return createMessage({ id: userId }, {}, (error, result) => {
         if (result) {
           return postMessage(result, channel, consoleOut);
@@ -50,7 +59,7 @@ http.createServer((request, response) => {
         return postMessage('sorry something went wrong', channel, consoleOut);
       });
     }
-    response.statusCode = 401;
-    return response.end('I can only handle parrot requests and challenges');
+    // handle an improperly formatted request
+    return postMessage('Please use the following format to request that I parrot someone, \n `@super_duper_bot parrot @PERSON`', channel, consoleOut);
   });
 }).listen(port, () => console.log(`listening on ${port}`));
